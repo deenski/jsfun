c47dfd0940ba7e7e000d91b28656e0c8df9c107d
deenski <jakevendegna@gmail.com>
2018-06-05 07:19:46 -0600

    starting on tictactoe game

---
+++ tictactoe/script.js
@@ -0,0 +1,9 @@
+document.getElementById("cell1").innerText = "X";
+document.getElementById("cell2").innerText = "O";
+document.getElementById("cell3").innerText = "O";
+document.getElementById("cell4").innerText = "O";
+document.getElementById("cell5").innerText = "O";
+document.getElementById("cell6").innerText = "O";
+document.getElementById("cell7").innerText = "O";
+document.getElementById("cell8").innerText = "O";
+document.getElementById("cell9").innerText = "O";
